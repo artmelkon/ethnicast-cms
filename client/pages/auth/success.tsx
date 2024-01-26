48df9a5b8ac27d510b8a3b811be0869c5289f2f3
artmelkon <arthur@artmelkon.me>
2024-01-25 17:22:00 -0800

    update css

--- client/pages/auth/success.tsx
+++ client/pages/auth/success.tsx
@@ -1,7 +1,7 @@
 export default function Success() {
   return (
-    <div>
-      <p style={{ textAlign: "center", fontSize: "2rem", color: "lightcoral" }}>
+    <div style={{ textAlign: "center" }}>
+      <p style={{ fontSize: "2rem", color: "lightcoral" }}>
         Your request sent successfully!
       </p>
       <p>Password change request sent to your email</p>
